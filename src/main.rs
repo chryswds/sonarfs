b682972d98638dcc552c9d293033e643b2c01bcf
Chrystian Dybas <dybas.chrys@gmail.com>
2026-06-10 19:09:03 +0100

    recursive folder scan returns disk usage

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use std::{fs, io};
 
 fn readable_size(bytes: u64) -> String {
     let base = 1024;
-
     let float_value = bytes as f64;
-
     if bytes < base {
         format!("{bytes} B")
     } else if bytes < base.pow(2) {
@@ -23,38 +21,28 @@ fn dir_size(dir_path: &Path) -> io::Result<u64> {
     let mut entries = fs::read_dir(dir_path)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>>>()?;
-
     entries.sort();
-
-    println!("{:?}", dir_path);
-
     let mut total_size: u64 = 0;
-
     for entry in &entries {
         let metadata = fs::metadata(entry)?;
-
-        let size = metadata.len();
-
-        let readable_size = readable_size(size);
-        total_size += size;
-
-        println!("{:?} - size = {}", entry, readable_size);
+        if metadata.is_dir() {
+            let folder_size = dir_size(entry)?;
+            total_size += folder_size;
+        } else {
+            let size = metadata.len();
+            total_size += size;
+        }
     }
-
-    let readable_total_size = readable_size(total_size);
-
-    println!("Total size = {}", readable_total_size);
-
     Ok(total_size)
 }
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-
     let dir_path_str = &args[1];
     let dir_path_path = Path::new(&dir_path_str);
-
-    let _total_size = dir_size(dir_path_path)?;
-
+    println!("{:?}", dir_path_path);
+    let total_size = dir_size(dir_path_path)?;
+    let readable_total_size = readable_size(total_size);
+    println!("{readable_total_size}");
     Ok(())
 }
