aad24bd39456dfaf1d1094c21b8635dc60039a8c
Chrystian Dybas <dybas.chrys@gmail.com>
2026-06-13 18:09:56 +0100

    adds struct

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,7 @@ fn entry_size(entry: &Path) -> u64 {
     }
 }
 
-// returs the size of a folder
+// returns the size of a folder
 fn dir_size(dir_path: &Path) -> io::Result<u64> {
     let entries = fs::read_dir(dir_path)?
         .map(|res| res.map(|e| e.path()))
@@ -48,45 +48,38 @@ struct Entry {
     path: PathBuf,
 }
 
-
-
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let path = Path::new(&args[1]);
     println!("{}", path.display());
-
-
     let mut display_rows: Option<usize> = None;
     if let Some(arg) = args.get(2)
         && arg == "--top"
     {
         display_rows = args.get(3).and_then(|s| s.parse::<usize>().ok());
     }
-
     let entries = fs::read_dir(path)?
         .map(|res| res.map(|e| e.path()))
         .collect::<Result<Vec<_>>>()?;
-
-    let entry = Entry {
-        size: entry_size(path),
-        path: path.to_path_buf(),
-    };
-
-    let mut entries_by_size: Vec<(u64, &PathBuf)> = entries.iter().map(|entry|(entry_size(entry), entry)).collect();
-
-    entries_by_size.sort_by_key(|(s, _)| std::cmp::Reverse(*s));
-
-
-    let total_size: u64 = entries_by_size.iter().map(|(size, _)| *size).sum();
-
-    for (size, path) in entries_by_size
+    let mut entries_size: Vec<Entry> = entries
+        .iter()
+        .map(|path| Entry {
+            size: entry_size(path),
+            path: path.to_path_buf(),
+        })
+        .collect();
+    entries_size.sort_by_key(|entry| std::cmp::Reverse(entry.size));
+    let total_size: u64 = entries_size.iter().map(|entry| entry.size).sum();
+    for entry in entries_size
         .iter()
         .take(display_rows.unwrap_or(usize::MAX))
     {
-        println!("{} ---- {}", path.display(), readable_size(*size));
+        println!(
+            "{} ---- {}",
+            entry.path.display(),
+            readable_size(entry.size)
+        );
     }
-
     println!("Total size - {}", readable_size(total_size));
-
     Ok(())
 }
